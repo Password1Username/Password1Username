f92980caf8182f6c1a71365ad22ca6b8c4a33770
Password1Username <deansmith1991@aol.de>
2019-11-25 20:51:36 +0100

    - tilemap got an own class - deleted a lot of code that was comment out - deleted not used variables - !! Music does not work now :Pink panther/

--- witchhunt/main.py
+++ witchhunt/main.py
@@ -7,40 +7,26 @@ import os
 sys.path.append(os.path.abspath('..'))
 import pygame
 from pygame.locals import *
-import pyganim
-import math as m
-from SpriteClass import  MySprite, MyPlayer
-
+from SpriteClass import MySprite, MyPlayer
+from tilemap import materials, Tilemap
 
 pygame.init()
 
-
-'''Tile map dimensions'''
-nwidth=16
-nheight=16
-
-tilewidth=32
-tilewidth_scaled=32
-tileheight=32
-tileheight_scaled=32
-
-
+tilemap = Tilemap()
 
 # set up the window
-smallwinx=tilewidth*nwidth
+smallwinx=tilemap.width*tilemap.nwidth
 winx=smallwinx
-smallwiny=tileheight*nheight
+smallwiny=tilemap.height*tilemap.nheight
 winy=smallwiny
 
 scalew=1.0
 scaleh=1.0
 
-
 xpos=0.5*winx
 ypos=0.5*winy
 xpos_scaled=xpos
 ypos_scaled=ypos
-
 
 ratex=0.01
 ratey=0.01
@@ -51,55 +37,44 @@ dy=winy*ratey
 housex=50
 housey=50
 
-
 windowSurface = pygame.display.set_mode((winx,winy), HWSURFACE|DOUBLEBUF|RESIZABLE, 32)
 pygame.display.set_caption('Witch Hunt')
 
-#create dummy house object
-#house = pygame.image.load('./pics/buildings/haus1.png')
-
 #First try for using the sprite class
 house_obj = MySprite('./pics/buildings/haus1.png', housex, housey)
-#house_obj
-#house_obj.
 
 sheetwidth=6*64
 sheetheight=2*40
 '''Picture offset'''
 spritey=0
 spritex=0
-spritewidth=64
-spriteheight=40
+spritewidth = 64
+spriteheight = 40
 
-rects_up   = [(  spritex+   3*spritewidth , spritey , spritewidth,spriteheight),
-              (  spritex+   4*spritewidth , spritey , spritewidth,spriteheight),
-              (  spritex+   5*spritewidth , spritey , spritewidth,spriteheight)]   
-
-rects_down = [(  spritex                  , spritey , spritewidth,spriteheight),
-              (  spritex+     spritewidth , spritey , spritewidth,spriteheight),
-              (  spritex+   2*spritewidth , spritey , spritewidth,spriteheight)]   
-
-rects_left = [(  spritex                  , spritey+spriteheight , spritewidth,spriteheight),
-              (  spritex+     spritewidth , spritey+spriteheight , spritewidth,spriteheight),
-              (  spritex+   2*spritewidth , spritey+spriteheight , spritewidth,spriteheight)]   
-
-rects_right= [(  spritex+   3*spritewidth , spritey+spriteheight , spritewidth,spriteheight),
-              (  spritex+   4*spritewidth , spritey+spriteheight , spritewidth,spriteheight),
-              (  spritex+   5*spritewidth , spritey+spriteheight , spritewidth,spriteheight)]   
-
-
-
-
-
+# rects_up   = [(  spritex+   3*spritewidth , spritey , spritewidth,spriteheight),
+#               (  spritex+   4*spritewidth , spritey , spritewidth,spriteheight),
+#               (  spritex+   5*spritewidth , spritey , spritewidth,spriteheight)]
+#
+# rects_down = [(  spritex                  , spritey , spritewidth,spriteheight),
+#               (  spritex+     spritewidth , spritey , spritewidth,spriteheight),
+#               (  spritex+   2*spritewidth , spritey , spritewidth,spriteheight)]
+#
+# rects_left = [(  spritex                  , spritey+spriteheight , spritewidth,spriteheight),
+#               (  spritex+     spritewidth , spritey+spriteheight , spritewidth,spriteheight),
+#               (  spritex+   2*spritewidth , spritey+spriteheight , spritewidth,spriteheight)]
+#
+# rects_right= [(  spritex+   3*spritewidth , spritey+spriteheight , spritewidth,spriteheight),
+#               (  spritex+   4*spritewidth , spritey+spriteheight , spritewidth,spriteheight),
+#               (  spritex+   5*spritewidth , spritey+spriteheight , spritewidth,spriteheight)]
 
 
 # playerAnim_scaled = playerAnim_down
 
-player = MyPlayer(xpos=0.0,ypos=0.0)
-player.setAnimation('./pics/blueboy_64_40.png', rects_down, "down")
-player.setAnimation('./pics/blueboy_64_40.png', rects_up, "up")
-player.setAnimation('./pics/blueboy_64_40.png', rects_left, "left")
-player.setAnimation('./pics/blueboy_64_40.png', rects_right, "right")
+player = MyPlayer(spritex, spritey, spritewidth, spriteheight, xpos=0.0,ypos=0.0)
+player.setAnimation('./pics/blueboy_64_40.png', "down")
+player.setAnimation('./pics/blueboy_64_40.png', "up")
+player.setAnimation('./pics/blueboy_64_40.png', "left")
+player.setAnimation('./pics/blueboy_64_40.png', "right")
 player.setInitAnimation("down")
 player.setXY(0.0, 0.0)
 player.setdxdy(dx, dy)
@@ -115,8 +90,6 @@ rects_down_danny = [(  spritex , spritey , spritewidth,spriteheight),
 danny.setAnimation('./pics/people/danny_64_40.png', rects_down_danny, "down")
 danny.setInitAnimation("down")
 danny.setXY(0.0, 0.0)
-#player.setdxdy(dx,dy)
-
 
 """Used to scale objects."""
 def scaled_variable(variable,scale):
@@ -130,27 +103,18 @@ def scaled_anim_object(ref_object,image_w,image_h,scale_w,scale_h):
 
 mainClock = pygame.time.Clock()
 '''Music'''
-pygame.mixer.music.load('./music/witch-hunt.wav')
+#pygame.mixer.music.load('./music/witch-hunt.wav')
 #pygame.mixer.music.load('./music/intro.wav')
-pygame.mixer.music.play(-1)
+#pygame.mixer.music.play(-1)
 
 FPS=30
 
 #pygame.key.set_repeat(100,1000)
 house_scaled=house_obj
 
-
 red = (100, 50, 50)
 grassgreen=(170,244,66)
 earthbrown=(230, 115, 0)
-
-
-grass=0
-dirt=1
-water=3
-hedge=4
-stone=5
-block=6
 
 '''Initialization of sprite objects'''
 grass_obj = MySprite('./pics/terrain/grass-map-periodic.png')
@@ -160,49 +124,21 @@ stone_obj = MySprite('./pics/boundaries/stone-map.png')
 hedge_obj = MySprite('./pics/boundaries/hedge-map-green.png')
 block_obj = MySprite('./pics/boundaries/block-map.png')
 
-                                     
 textures={
-        grass: grass_obj,
-        dirt: dirt_obj,
-        stone: stone_obj,
-        water: water_obj,
-        hedge: hedge_obj,
-        block: block_obj
+        materials["grass"]: grass_obj,
+        materials["dirt"]: dirt_obj,
+        materials["stone"]: stone_obj,
+        materials["water"]: water_obj,
+        materials["hedge"]: hedge_obj,
+        materials["block"]: block_obj
         }
 
-tilemap=[[grass,grass,grass,grass,grass,grass,block,dirt ,block,grass,grass,grass,grass,grass,grass,grass],
-         [grass,grass,grass,grass,grass,grass,grass,dirt ,grass,grass,grass,grass,grass,grass,grass,grass],
-         [grass,grass,grass,grass,grass,grass,grass,dirt ,hedge,grass,grass,grass,grass,grass,grass,grass],
-         [grass,grass,grass,grass,grass,grass,hedge,dirt ,grass,grass,grass,grass,grass,grass,grass,grass], 
-         [grass,grass,grass,grass,grass,grass,grass,dirt ,grass,grass,grass,grass,grass,grass,grass,grass],
-         [grass,grass,grass,grass,grass,grass,grass,dirt ,grass,grass,grass,grass,grass,grass,grass,grass],
-         [grass,grass,grass,grass,grass,grass,grass,dirt ,hedge,grass,grass,grass,grass,grass,grass,grass],
-         [grass,grass,grass,grass,grass,grass,hedge,dirt ,grass,grass,grass,grass,grass,grass,grass,grass],
-         [grass,grass,grass,grass,grass,grass,grass,dirt ,grass,grass,grass,grass,grass,grass,grass,grass],
-         [grass,grass,grass,grass,grass,grass,grass,dirt ,grass,grass,grass,grass,grass,grass,grass,grass],
-         [grass,grass,grass,grass,grass,grass,grass,dirt ,hedge,grass,grass,grass,grass,grass,grass,grass],
-         [grass,grass,grass,grass,grass,grass,hedge,dirt ,grass,grass,grass,grass,grass,grass,grass,grass],
-         [grass,grass,grass,grass,grass,stone,water,water,water,stone,grass,grass,grass,grass,grass,grass],
-         [grass,grass,grass,grass,grass,stone,water,water,water,stone,grass,grass,grass,grass,grass,grass],
-         [grass,grass,grass,grass,grass,hedge,water,water,water,hedge,grass,grass,grass,grass,grass,grass],
-         [grass,grass,grass,grass,grass,hedge,hedge,hedge,hedge,hedge,grass,grass,grass,grass,grass,grass]]
-
-for row in range(0, nheight):
-    for column in range(0, nwidth):
-        # print str(row)+","+str(column)
-        # print(tilemap[row][column])
-        # print(block_obj.getCollision(house_obj))
-
-        textures[tilemap[row][column]].setXY(round(column * tilewidth_scaled), round(row * tileheight_scaled))
-
-
-
-
+for row in range(0, tilemap.nheight):
+    for column in range(0, tilemap.nwidth):
+        textures[tilemap.current[row][column]].setXY(round(column * tilemap.width_scaled), round(row * tilemap.height_scaled))
 
 while True:
     windowSurface.fill(grassgreen)
-    #scaleh=1.0
-    #scalew=1.0
     for event in pygame.event.get():
         if event.type == QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE):
             pygame.quit()
@@ -214,7 +150,6 @@ while True:
         '''
         if event.type == pygame.VIDEORESIZE:
             # There's some code to add back window content here.
-     
             tempSurface = pygame.display.set_mode((event.w, event.h),HWSURFACE|DOUBLEBUF|RESIZABLE)
             windowSurface.blit(tempSurface, (0,0))
             windowSurface = tempSurface
@@ -223,59 +158,29 @@ while True:
             del tempSurface
             scalew=float(winx)/smallwinx
             scaleh=float(winy)/smallwiny
-            tilewidth_scaled=round(scaled_variable(tilewidth,scalew))
-            tileheight_scaled=round(scaled_variable(tileheight,scaleh))
-            #textures[tilemap[row][column]].image_rect
-            #textures={key: pygame.transform.scale(textures[key] , (int(tilewidth_scaled),int(tileheight_scaled))) for (key,textures[key]) in textures.items()}
-            for row in range(0, nheight):
-                for column in range(0, nwidth):
-                    textures[tilemap[row][column]].setXY(column * tilewidth_scaled, row * tileheight_scaled)
-                    textures[tilemap[row][column]].scaleValues(scalew,scaleh)
-                    #print(textures[tilemap[row][column]].image_rect)
-            #xpos_scaled=scaled_variable(xpos,scalew)
-            #ypos_scaled=scaled_variable(ypos,scaleh)
-
+            tilewidth_scaled=round(scaled_variable(tilemap.width,scalew))
+            tileheight_scaled=round(scaled_variable(tilemap.height,scaleh))
+            for row in range(0, tilemap.nheight):
+                for column in range(0, tilemap.nwidth):
+                    textures[tilemap.current[row][column]].setXY(column * tilewidth_scaled, row * tileheight_scaled)
+                    textures[tilemap.current[row][column]].scaleValues(scalew,scaleh)
             house_scaled=house_obj
             house_scaled.scaleValues(scalew,scaleh)
 
-            #print player.Animation_scaled
             player.setScale(scalew, scaleh)
             player.scaleValues()
-            #player.move(event, windowSurface)
 
             danny.setScale(scalew, scaleh)
             danny.scaleValues()
-            #danny.playAnim"down"]
-            #danny.move(event, windowSurface)
 
     '''Inter'''
-    for row in range(0,nheight):
-        for column in range(0,nwidth):
-            #print str(row)+","+str(column)
-            #print(tilemap[row][column])
-            #print(block_obj.getCollision(house_obj))
-            textures[tilemap[row][column]].setXY(round(column * tilewidth_scaled), round(row * tileheight_scaled))
-            #textures[tilemap[row][column]].image_rect=(round(column * tilewidth_scaled), round(row * tileheight_scaled), m.ceil(tilewidth_scaled),m.ceil(tileheight_scaled))
-            #print(textures[tilemap[row][column]].image_rect)
-            windowSurface.blit(textures[tilemap[row][column]].image_obj,textures[tilemap[row][column]].image_rect)
+    for row in range(0, tilemap.nheight):
+        for column in range(0,tilemap.nwidth):
+            textures[tilemap.current[row][column]].setXY(round(column * tilemap.width_scaled), round(row * tilemap.height_scaled))
+            windowSurface.blit(textures[tilemap.current[row][column]].image_obj,textures[tilemap.current[row][column]].image_rect)
 
-
-    # print(event)
-    #print player.x_scaled, player.y_scaled
-    #windowSurface.blit(house_scaled.image_obj, (house_scaled.x_scaled,house_scaled.y_scaled)) #display house on
-    #player.Anim_scaled.blit(windowSurface, (player.x_scaled,player.y_scaled))
     danny.playAnim(windowSurface, "down")
-    #print(danny.x_scaled,danny.y_scaled)
     player.move(event, windowSurface)
-    #player.getInputState(event)
 
-    #print(player.x_scaled, player.y_scaled)
-
-    #print(danny.x_scaled)
-    #danny.move(event,windowSurface)
-    #player.Anim_scaled.blit(windowSurface, (player.x_scaled, player.y_scaled))
     pygame.display.update()
     mainClock.tick(FPS) 
-
-
-
---
+++ witchhunt/tilemap.py
@@ -0,0 +1,50 @@
+materials = {"grass": 0, "dirt": 1, "water": 3, "hedge": 4, "stone": 5, "block": 6}
+
+class Tilemap():
+
+    def __init__(self):
+        self.nwidth = 16
+        self.nheight = 16
+
+        self.width = 32
+        self.width_scaled = 32
+        self.height = 32
+        self.height_scaled = 32
+
+        self.current = self.tilemap_one()
+
+    def tilemap_one(self):
+        tilemap = [
+            [materials["grass"], materials["grass"], materials["grass"], materials["grass"], materials["grass"], materials["grass"], materials["block"], materials["dirt"], materials["block"], materials["grass"], materials["grass"], materials["grass"], materials["grass"], materials["grass"], materials["grass"],
+             materials["grass"]],
+            [materials["grass"], materials["grass"], materials["grass"], materials["grass"], materials["grass"], materials["grass"], materials["grass"], materials["dirt"], materials["grass"], materials["grass"], materials["grass"], materials["grass"], materials["grass"], materials["grass"], materials["grass"],
+             materials["grass"]],
+            [materials["grass"], materials["grass"], materials["grass"], materials["grass"], materials["grass"], materials["grass"], materials["grass"], materials["dirt"], materials["hedge"], materials["grass"], materials["grass"], materials["grass"], materials["grass"], materials["grass"], materials["grass"],
+             materials["grass"]],
+            [materials["grass"], materials["grass"], materials["grass"], materials["grass"], materials["grass"], materials["grass"], materials["hedge"], materials["dirt"], materials["grass"], materials["grass"], materials["grass"], materials["grass"], materials["grass"], materials["grass"], materials["grass"],
+             materials["grass"]],
+            [materials["grass"], materials["grass"], materials["grass"], materials["grass"], materials["grass"], materials["grass"], materials["grass"], materials["dirt"], materials["grass"], materials["grass"], materials["grass"], materials["grass"], materials["grass"], materials["grass"], materials["grass"],
+             materials["grass"]],
+            [materials["grass"], materials["grass"], materials["grass"], materials["grass"], materials["grass"], materials["grass"], materials["grass"], materials["dirt"], materials["grass"], materials["grass"], materials["grass"], materials["grass"], materials["grass"], materials["grass"], materials["grass"],
+             materials["grass"]],
+            [materials["grass"], materials["grass"], materials["grass"], materials["grass"], materials["grass"], materials["grass"], materials["grass"], materials["dirt"], materials["hedge"], materials["grass"], materials["grass"], materials["grass"], materials["grass"], materials["grass"], materials["grass"],
+             materials["grass"]],
+            [materials["grass"], materials["grass"], materials["grass"], materials["grass"], materials["grass"], materials["grass"], materials["hedge"], materials["dirt"], materials["grass"], materials["grass"], materials["grass"], materials["grass"], materials["grass"], materials["grass"], materials["grass"],
+             materials["grass"]],
+            [materials["grass"], materials["grass"], materials["grass"], materials["grass"], materials["grass"], materials["grass"], materials["grass"], materials["dirt"], materials["grass"], materials["grass"], materials["grass"], materials["grass"], materials["grass"], materials["grass"], materials["grass"],
+             materials["grass"]],
+            [materials["grass"], materials["grass"], materials["grass"], materials["grass"], materials["grass"], materials["grass"], materials["grass"], materials["dirt"], materials["grass"], materials["grass"], materials["grass"], materials["grass"], materials["grass"], materials["grass"], materials["grass"],
+             materials["grass"]],
+            [materials["grass"], materials["grass"], materials["grass"], materials["grass"], materials["grass"], materials["grass"], materials["grass"], materials["dirt"], materials["hedge"], materials["grass"], materials["grass"], materials["grass"], materials["grass"], materials["grass"], materials["grass"],
+             materials["grass"]],
+            [materials["grass"], materials["grass"], materials["grass"], materials["grass"], materials["grass"], materials["grass"], materials["hedge"], materials["dirt"], materials["grass"], materials["grass"], materials["grass"], materials["grass"], materials["grass"], materials["grass"], materials["grass"],
+             materials["grass"]],
+            [materials["grass"], materials["grass"], materials["grass"], materials["grass"], materials["grass"], materials["stone"], materials["water"], materials["water"], materials["water"], materials["stone"], materials["grass"], materials["grass"], materials["grass"], materials["grass"], materials["grass"],
+             materials["grass"]],
+            [materials["grass"], materials["grass"], materials["grass"], materials["grass"], materials["grass"], materials["stone"], materials["water"], materials["water"], materials["water"], materials["stone"], materials["grass"], materials["grass"], materials["grass"], materials["grass"], materials["grass"],
+             materials["grass"]],
+            [materials["grass"], materials["grass"], materials["grass"], materials["grass"], materials["grass"], materials["hedge"], materials["water"], materials["water"], materials["water"], materials["hedge"], materials["grass"], materials["grass"], materials["grass"], materials["grass"], materials["grass"],
+             materials["grass"]],
+            [materials["grass"], materials["grass"], materials["grass"], materials["grass"], materials["grass"], materials["hedge"], materials["hedge"], materials["hedge"], materials["hedge"], materials["hedge"], materials["grass"], materials["grass"], materials["grass"], materials["grass"], materials["grass"],
+             materials["grass"]]]
+        return tilemap
