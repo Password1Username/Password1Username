af043f1d154ad366d56d679722e9e32893eb5323
Password1Username <deansmith1991@aol.de>
2020-06-02 01:33:15 +0200

    Less buggy collision detection for normal screen, no collision detection for scaled screen.
    Signed-off-by: des <deansmith1991@aol.de>

--- witchhunt/SpriteClass.py
+++ witchhunt/SpriteClass.py
@@ -12,7 +12,7 @@ class Input:
         self.timeSinceInput = None
 
 
-class Inputs():
+class Inputs:
 
     def __init__(self):
         self.bindings = {"up": pygame.K_UP,
@@ -96,61 +96,91 @@ class Inputs():
 
         return self.keyState
 
+'''Class storing parameters'''
 
-'''Sprite Class'''
-
-
-class MySprite():
+class MyObject():
     ''' The constructor of class '''
 
-    def __init__(self, imagePath, xpos=0.0, ypos=0.0):
-        # ''' Load image object '''
-        self.image_obj_original = pygame.image.load(imagePath)
-        self.image_obj = pygame.image.load(imagePath)
-        self.image_rect = self.image_obj.get_rect()
+    def __init__(self, xpos=0.0, ypos=0.0):
+
         '''Scaling factors'''
         self.scaleinx = 1.0
         self.scaleiny = 1.0
-        ''' Image parameters '''
-        ''' Case: xpos passed in constructor '''
+
+        # ''' Case: xpos passed in constructor '''
         self.x = xpos
         # '''Case: ypos passed in constructor'''
         self.y = ypos
-        self.width = self.image_rect[2]
-        self.height = self.image_rect[3]
-        self.image_rect = pygame.Rect(self.x, self.y, self.width, self.height)
-        # Set all image values
-        # self.getImageValues()
+
         self.dx = 0.0
         self.dy = 0.0
-        '''Collision parameters'''
-        self.collision_x1 = self.x
-        self.collision_y1 = self.y
-        self.collision_width = self.width
-        self.collision_height = self.height
-        self.collision_rect = self.image_rect
-
-        self.collision_x2 = self.collision_x1 + self.collision_width
-        self.collision_y2 = self.collision_y1 + self.collision_height
 
         '''Scaled parameters'''
         self.x_scaled = self.x * self.scaleinx
         self.y_scaled = self.y * self.scaleiny
+
+
+        '''Collision parameters'''
+        self.collision_x1 = self.x
+        self.collision_y1 = self.y
+
+
+        self.dx_scaled = self.dx*self.scaleinx
+        self.dy_scaled = self.dx*self.scaleiny
+
+        self.collision_x1_scaled = self.x_scaled
+        self.collision_y1_scaled = self.y_scaled
+
+'''Sprite Class'''
+
+
+class MySprite(MyObject):
+    ''' The constructor of class '''
+
+    def __init__(self, imagePath, xpos=0.0, ypos=0.0):
+        super().__init__()
+        # ''' Load image object '''
+        self.image_obj_original = pygame.image.load(imagePath)
+        self.image_obj = pygame.image.load(imagePath)
+        self.image_rect = self.image_obj.get_rect()
+
+        ''' Image parameters '''
+        self.width = self.image_rect[2]
+        self.height = self.image_rect[3]
+        self.image_rect = pygame.Rect(self.x, self.y, self.width, self.height)
+
+        # Set all image values
+        # self.getImageValues()
+        self.collision_width = self.width
+        self.collision_height = self.height
+
+        self.collision_x2 = self.collision_x1 + self.collision_width
+        self.collision_y2 = self.collision_y1 + self.collision_height
+
+        self.collision_center_x = self.collision_x1 + 0.5*self.collision_width
+        self.collision_center_y = self.collision_y1 + 0.5*self.collision_height
+        self.collision_rect = self.image_rect
+
+        # '''Scaled quantities'''
+
         self.width_scaled = self.width * self.scaleinx
         self.height_scaled = self.height * self.scaleiny
         self.image_rect_scaled = pygame.Rect(self.x_scaled, self.y_scaled, self.width_scaled, self.height_scaled)
-        self.dx_scaled = 0.0
-        self.dy_scaled = 0.0
+
         # print(self.width)
         # exit()
 
-        self.collision_x1_scaled = self.x_scaled
+
         self.collision_x2_scaled = self.collision_x1_scaled + self.width_scaled
-        self.collision_y1_scaled = self.y_scaled
+
         self.collision_y2_scaled = self.collision_y1_scaled + self.height_scaled
         self.collision_width_scaled = self.width_scaled
         self.collision_height_scaled = self.height_scaled
         self.collision_rect_scaled = self.image_rect_scaled
+
+        self.collision_center_x_scaled = self.collision_x1_scaled + 0.5*self.collision_width_scaled
+        self.collision_center_y_scaled = self.collision_y1_scaled + 0.5*self.collision_height_scaled
+
 
     def setXY(self, valx, valy):
         self.x = valx
@@ -235,6 +265,9 @@ class MySprite():
         # print(rect ,self.collision_rect_scaled)
 
 
+
+
+
 class MyPlayer(Inputs):
 
     def __init__(self, xpos=0.0, ypos=0.0):
@@ -282,6 +315,7 @@ class MyPlayer(Inputs):
         self.image_rect = pygame.Rect(self.x, self.y, self.width, self.height)
         self.image_rect_scaled = pygame.Rect(self.x_scaled, self.y_scaled, self.width_scaled, self.height_scaled)
 
+
         '''Collision parameters'''
         self.collision_x1 = self.x
         self.collision_x2 = self.collision_x1 + self.width
@@ -290,6 +324,8 @@ class MyPlayer(Inputs):
         self.collision_width = self.width
         self.collision_height = self.height
         self.collision_rect = self.image_rect
+        self.collision_center_x = self.x + 0.5 * self.width
+        self.collision_center_y = self.y + 0.5 * self.height
 
         self.collision_x1_scaled = self.x_scaled
         self.collision_x2_scaled = self.collision_x1_scaled + self.width_scaled
@@ -298,6 +334,8 @@ class MyPlayer(Inputs):
         self.collision_width_scaled = self.width_scaled
         self.collision_height_scaled = self.height_scaled
         self.collision_rect_scaled = self.image_rect_scaled
+        self.collision_center_x_scaled = self.x_scaled + 0.5 * self.width_scaled
+        self.collision_center_y_scaled = self.y_scaled + 0.5 * self.height_scaled
 
     def setAnimation(self, spritesheet, frames, name):
         # print frames
@@ -315,6 +353,7 @@ class MyPlayer(Inputs):
             self.collision_x1_scaled = self.x * self.scaleinx
             self.collision_x2_scaled = self.x_scaled + self.width_scaled
             self.collision_width_scaled = self.width * self.scaleinx
+
 
         if not self.y:
             self.y = frames[0][3]
@@ -367,20 +406,37 @@ class MyPlayer(Inputs):
         self.collision_rect_scaled = pygame.Rect(self.collision_x1_scaled, self.collision_y1_scaled,
                                                  self.collision_width_scaled, self.collision_height_scaled)
 
+
     def collision_with(self, sprite):
-        # print("player " + str(self.collision_rect), "sprite " + str(sprite.collision_rect))
+        # print("player " + str(self.collision_rect))
+        #print("player " + str(self.collision_rect), "sprite " + str(sprite.collision_rect))
         # print(self.collision_rect)
         value=self.collision_rect.colliderect(sprite.collision_rect)
         if value==1:
             # if self.dx>0:
-            if self.incr_x > 0:  # Moving right; Hit the left side of the wall
+            # print("player " + str(self.collision_rect), "sprite " + str(sprite.collision_rect))
+            if sprite.collision_x1 < self.collision_x2:  # Moving right; Hit the left side of the wall
+
                 self.setX(sprite.collision_x1-self.collision_width)
-            elif self.incr_x < 0: # Moving left; Hit the right side of the wall
+                self.setCollisionPos(sprite.collision_x1-self.collision_width,sprite.collision_x2,self.collision_y1,self.collision_y2)
+                # print("player " + str(self.collision_rect), "sprite " + str(sprite.collision_rect))
+                # print("")
+
+            elif sprite.collision_x2 > self.collision_x1: # Moving left; Hit the right side of the wall
                 self.setX(sprite.collision_x2)
-            if self.incr_y > 0:  # Moving down; Hit the top side of the wall
+                self.setCollisionPos(sprite.collision_x2,sprite.collision_x2+self.collision_width, self.collision_y1,
+                                     self.collision_y2)
+
+            if sprite.collision_y1 < self.collision_y2:  # Moving down; Hit the top side of the wall
+                # print("player " + str(self.collision_rect), "sprite " + str(sprite.collision_rect))
                 self.setY(sprite.collision_y1-self.collision_height)
-            elif self.incr_y < 0:  # Moving up; Hit the bottom side of the wall
+                self.setCollisionPos(sprite.collision_x1, sprite.collision_x2, sprite.collision_y1-self.collision_height,
+                                     sprite.collision_y1)
+                # print("player " + str(self.collision_rect), "sprite " + str(sprite.collision_rect))
+            elif sprite.collision_y2 > self.collision_y1:  # Moving up; Hit the bottom side of the wall
+
                 self.setY(sprite.collision_y2)
+                self.setCollisionPos(sprite.collision_x1,sprite.collision_x2, sprite.collision_y1,sprite.collision_y2+self.collision_height)
         return value
 
     def setX(self,valx):
@@ -532,14 +588,17 @@ class MyPlayer(Inputs):
             self.incr_y = 0.0
             self.incr_x = 0.0
             self.Anim_scaled.pause()
-
+        deltax = self.dscale * self.incr_x
+        deltay = self.dscale * self.incr_y
         self.x = round(self.x + self.dscale * self.incr_x)
         self.y = round(self.y + self.dscale * self.incr_y)
         self.x_scaled = self.x * self.scaleinx
         self.y_scaled = self.y * self.scaleiny
         # print(self.collision_width,self.collision_height)
-        self.setCollisionPos(self.x, self.x + self.collision_width, self.y, self.y + self.collision_height)
+        self.setCollisionPos(deltax + self.collision_x1, deltax + self.collision_x2, deltay+self.collision_y1, deltay+self.collision_y2)
         self.Anim_scaled.blit(surface, (self.x_scaled, self.y_scaled))
+        pygame.draw.rect(surface, (0, 0, 0), Rect(self.collision_x1_scaled, self.collision_y1_scaled,
+                                                  self.collision_width_scaled, self.collision_width_scaled))
 
     def playAnim(self, surface, name):
         # self.Animation_scaled[name].play()#.blit(surface, (self.x_scaled, self.y_scaled))
@@ -550,3 +609,5 @@ class MyPlayer(Inputs):
         self.Anim_scaled.play()
         # print(self.x_scaled, self.y_scaled)
         self.Anim_scaled.blit(surface, (self.x_scaled, self.y_scaled))
+
+
--- witchhunt/main.py
+++ witchhunt/main.py
@@ -4,111 +4,116 @@
 
 import sys
 import os
+
 sys.path.append(os.path.abspath('..'))
 import pygame
 from pygame.locals import *
 import pyganim
 import math as m
-from SpriteClass import  MySprite, MyPlayer
+from SpriteClass import MySprite, MyPlayer
 from tilemap import materials, Tilemap
-
-
 
 pygame.init()
 
-
 '''Tile map dimensions'''
-nwidth=16
-nheight=16
+nwidth = 16
+nheight = 16
 
-tilewidth=32
-tilewidth_scaled=32
-tileheight=32
-tileheight_scaled=32
-
-
+tilewidth = 32
+tilewidth_scaled = 32
+tileheight = 32
+tileheight_scaled = 32
 
 # set up the window
-smallwinx=tilewidth*nwidth
-winx=smallwinx
-smallwiny=tileheight*nheight
-winy=smallwiny
+smallwinx = tilewidth * nwidth
+winx = smallwinx
+smallwiny = tileheight * nheight
+winy = smallwiny
 
-scalew=1.0
-scaleh=1.0
+scalew = 1.0
+scaleh = 1.0
 
+xpos = 0.5 * winx
+ypos = 0.5 * winy
+xpos_scaled = xpos
+ypos_scaled = ypos
 
-xpos=0.5*winx
-ypos=0.5*winy
-xpos_scaled=xpos
-ypos_scaled=ypos
+ratex = 0.01
+ratey = 0.01
 
+dx = winx * ratex
+dy = winy * ratey
 
-ratex=0.01
-ratey=0.01
+housex = 50
+housey = 50
 
-dx=winx*ratex
-dy=winy*ratey
-
-housex=50
-housey=50
-
-
-windowSurface = pygame.display.set_mode((winx,winy), HWSURFACE|DOUBLEBUF|RESIZABLE, 32)
+windowSurface = pygame.display.set_mode((winx, winy), HWSURFACE | DOUBLEBUF | RESIZABLE, 32)
 pygame.display.set_caption('Witch Hunt')
 #
-#create dummy house object
-#house = pygame.image.load('./pics/buildings/haus1.png')
+# create dummy house object
+# house = pygame.image.load('./pics/buildings/haus1.png')
 
-#First try for using the sprite class
+# First try for using the sprite class
 house_obj = MySprite('./pics/buildings/haus1.png', housex, housey)
-#house_obj
-#house_obj.
+# house_obj
+# house_obj.
 
-sheetwidth=6*64
-sheetheight=2*40
+sheetwidth = 6 * 64
+sheetheight = 2 * 40
 '''Picture offset'''
-spritey=0
-spritex=0
-spritewidth=64
-spriteheight=40
+spritey = 0
+spritex = 0
+spritewidth = 64
+spriteheight = 40
 
-rects_up   = [(  spritex+   3*spritewidth , spritey , spritewidth,spriteheight),
-              (  spritex+   4*spritewidth , spritey , spritewidth,spriteheight),
-              (  spritex+   5*spritewidth , spritey , spritewidth,spriteheight)]   
+rects_up = [(spritex + 3 * spritewidth, spritey, spritewidth, spriteheight),
+            (spritex + 4 * spritewidth, spritey, spritewidth, spriteheight),
+            (spritex + 5 * spritewidth, spritey, spritewidth, spriteheight)]
 
-rects_down = [(  spritex                  , spritey , spritewidth,spriteheight),
-              (  spritex+     spritewidth , spritey , spritewidth,spriteheight),
-              (  spritex+   2*spritewidth , spritey , spritewidth,spriteheight)]   
+rects_down = [(spritex, spritey, spritewidth, spriteheight),
+              (spritex + spritewidth, spritey, spritewidth, spriteheight),
+              (spritex + 2 * spritewidth, spritey, spritewidth, spriteheight)]
 
-rects_left = [(  spritex                  , spritey+spriteheight , spritewidth,spriteheight),
-              (  spritex+     spritewidth , spritey+spriteheight , spritewidth,spriteheight),
-              (  spritex+   2*spritewidth , spritey+spriteheight , spritewidth,spriteheight)]   
+rects_left = [(spritex, spritey + spriteheight, spritewidth, spriteheight),
+              (spritex + spritewidth, spritey + spriteheight, spritewidth, spriteheight),
+              (spritex + 2 * spritewidth, spritey + spriteheight, spritewidth, spriteheight)]
 
-rects_right= [(  spritex+   3*spritewidth , spritey+spriteheight , spritewidth,spriteheight),
-              (  spritex+   4*spritewidth , spritey+spriteheight , spritewidth,spriteheight),
-              (  spritex+   5*spritewidth , spritey+spriteheight , spritewidth,spriteheight)]   
-
+rects_right = [(spritex + 3 * spritewidth, spritey + spriteheight, spritewidth, spriteheight),
+               (spritex + 4 * spritewidth, spritey + spriteheight, spritewidth, spriteheight),
+               (spritex + 5 * spritewidth, spritey + spriteheight, spritewidth, spriteheight)]
 
 # playerAnim_scaled = playerAnim_down
 
-player = MyPlayer(xpos=0.0,ypos=0.0)
+player = MyPlayer(xpos=0.0, ypos=0.0)
 player.setAnimation('./pics/blueboy_64_40.png', rects_down, "down")
 player.setAnimation('./pics/blueboy_64_40.png', rects_up, "up")
 player.setAnimation('./pics/blueboy_64_40.png', rects_left, "left")
 player.setAnimation('./pics/blueboy_64_40.png', rects_right, "right")
 player.setInitAnimation("down")
-player.setXY(12.0, 12.0)
+
+player_initx = 0.0
+player_inity = 0.0
+
+player.setXY(player_initx, player_inity)
+
+hitbox_2 = 16
+col_initx1 = player.collision_center_x - hitbox_2
+col_initx2 = player.collision_center_x + hitbox_2
+col_inity1 = player.collision_center_y - hitbox_2
+col_inity2 = player.collision_center_y + hitbox_2
+
+player.setCollisionPos(col_initx1, col_initx2, col_inity1, col_inity2)
+# print(player.collision_rect)
+# exit()
 # print(player.x_scaled, player.y_scaled)
 player.setdxdy(dx, dy)
 
+danny = MyPlayer(xpos=0, ypos=0)
 
-danny = MyPlayer(xpos=0,ypos=0)
-
-rects_down_danny = [(  spritex , spritey , spritewidth,spriteheight),
-              (  spritex+     spritewidth , spritey , spritewidth,spriteheight),
-              (  spritex+   2*spritewidth , spritey , spritewidth,spriteheight),
-            (  spritex+   3*spritewidth , spritey , spritewidth,spriteheight)]
+rects_down_danny = [(spritex, spritey, spritewidth, spriteheight),
+                    (spritex + spritewidth, spritey, spritewidth, spriteheight),
+                    (spritex + 2 * spritewidth, spritey, spritewidth, spriteheight),
+                    (spritex + 3 * spritewidth, spritey, spritewidth, spriteheight)]
 
 danny.setAnimation('./pics/people/danny_64_40.png', rects_down_danny, "down")
 danny.setInitAnimation("down")
@@ -118,97 +123,75 @@ danny.setXY(0.0, 100.0)
 
 
 """Used to scale objects."""
-def scaled_variable(variable,scale):
-    return scale*variable
 
-def scaled_anim_object(ref_object,image_w,image_h,scale_w,scale_h):
-    scaled_object=ref_object 
-    scaled_object.smoothscale((round(scale_w*image_w),round(scale_h*image_h)))
+
+def scaled_variable(variable, scale):
+    return scale * variable
+
+
+def scaled_anim_object(ref_object, image_w, image_h, scale_w, scale_h):
+    scaled_object = ref_object
+    scaled_object.smoothscale((round(scale_w * image_w), round(scale_h * image_h)))
     return scaled_object
+
 
 #
 mainClock = pygame.time.Clock()
 '''Music'''
 pygame.mixer.music.load('./music/witch-hunt.wav')
 # pygame.mixer.music.load('./music/intro.wav')
-pygame.mixer.music.play(-1)
+pygame.mixer.music.play(0)
 
-FPS=30
+FPS = 30
 
 # pygame.key.set_repeat(100,1000)
-house_scaled=house_obj
-
+house_scaled = house_obj
 
 red = (100, 50, 50)
-grassgreen=(170,244,66)
-earthbrown=(230, 115, 0)
+grassgreen = (170, 244, 66)
+earthbrown = (230, 115, 0)
 
-
-grass=0
-dirt=1
-water=3
-hedge=4
-stone=5
-block=6
+grass = 0
+dirt = 1
+water = 3
+hedge = 4
+stone = 5
+block = 6
 
 '''Initialization of sprite objects'''
 grass_obj = MySprite('./pics/terrain/grass-map-periodic.png')
-dirt_obj  = MySprite('./pics/terrain/dirt-map.png')
+dirt_obj = MySprite('./pics/terrain/dirt-map.png')
 water_obj = MySprite('./pics/terrain/water-map.png')
 stone_obj = MySprite('./pics/boundaries/stone-map.png')
 hedge_obj = MySprite('./pics/boundaries/hedge-map-green.png')
 block_obj = MySprite('./pics/boundaries/block-map.png')
 
-                                     
-textures={
-        grass: grass_obj,
-        dirt: dirt_obj,
-        stone: stone_obj,
-        water: water_obj,
-        hedge: hedge_obj,
-        block: block_obj
-        }
+textures = {
+    grass: grass_obj,
+    dirt: dirt_obj,
+    stone: stone_obj,
+    water: water_obj,
+    hedge: hedge_obj,
+    block: block_obj
+}
 
-block_textures={
+block_textures = {
     stone: stone_obj,
     water: water_obj,
     hedge: hedge_obj,
     block: block_obj}
 
-
-tilemap=[[grass,grass,grass,grass,grass,grass,block,dirt ,block,grass,grass,grass,grass,grass,grass,grass],
-         [grass,grass,grass,grass,grass,grass,grass,dirt ,grass,grass,grass,grass,grass,grass,grass,grass],
-         [grass,grass,grass,grass,grass,grass,grass,dirt ,hedge,grass,grass,grass,grass,grass,grass,grass],
-         [grass,grass,grass,grass,grass,grass,hedge,dirt ,grass,grass,grass,grass,grass,grass,grass,grass],
-         [grass,grass,grass,grass,grass,grass,grass,dirt ,grass,grass,grass,grass,grass,grass,grass,grass],
-         [grass,grass,grass,grass,grass,grass,grass,dirt ,grass,grass,grass,grass,grass,grass,grass,grass],
-         [grass,grass,grass,grass,grass,grass,grass,dirt ,hedge,grass,grass,grass,grass,grass,grass,grass],
-         [grass,grass,grass,grass,grass,grass,hedge,dirt ,grass,grass,grass,grass,grass,grass,grass,grass],
-         [grass,grass,grass,grass,grass,grass,grass,dirt ,grass,grass,grass,grass,grass,grass,grass,grass],
-         [grass,grass,grass,grass,grass,grass,grass,dirt ,grass,grass,grass,grass,grass,grass,grass,grass],
-         [grass,grass,grass,grass,grass,grass,grass,dirt ,hedge,grass,grass,grass,grass,grass,grass,grass],
-         [grass,grass,grass,grass,grass,grass,hedge,dirt ,grass,grass,grass,grass,grass,grass,grass,grass],
-         [grass,grass,grass,grass,grass,stone,water,water,water,stone,grass,grass,grass,grass,grass,grass],
-         [grass,grass,grass,grass,grass,stone,water,water,water,stone,grass,grass,grass,grass,grass,grass],
-         [grass,grass,grass,grass,grass,hedge,water,water,water,hedge,grass,grass,grass,grass,grass,grass],
-         [grass,grass,grass,grass,grass,hedge,hedge,hedge,hedge,hedge,grass,grass,grass,grass,grass,grass]]
-
-
-# tilemap = Tilemap()
+tilemap = Tilemap().tilemap_one()
 
 for row in range(0, nheight):
     for column in range(0, nwidth):
-        # print str(row)+","+str(column)
-        # print(tilemap[row][column])
-        # print(block_obj.getCollision(house_obj))
-
-        textures[tilemap[row][column]].setXY(round(column * tilewidth_scaled), round(row * tileheight_scaled))
-
+        textures[tilemap[row][column]].setXY(round(column * tilewidth), round(row * tileheight))
+        # textures[tilemap[row][column]].setXY(round(column * tilewidth_scaled), round(row * tileheight_scaled))
 
 while True:
     windowSurface.fill(grassgreen)
-    #scaleh=1.0
-    #scalew=1.0
+    # scaleh=1.0
+    # scalew=1.0
     for event in pygame.event.get():
         if event.type == QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE):
             pygame.quit()
@@ -220,25 +203,25 @@ while True:
         '''
         if event.type == pygame.VIDEORESIZE:
             # There's some code to add back window content here.
-     
-            tempSurface = pygame.display.set_mode((event.w, event.h),HWSURFACE|DOUBLEBUF|RESIZABLE)
-            windowSurface.blit(tempSurface, (0,0))
+
+            tempSurface = pygame.display.set_mode((event.w, event.h), HWSURFACE | DOUBLEBUF | RESIZABLE)
+            windowSurface.blit(tempSurface, (0, 0))
             windowSurface = tempSurface
             winx = windowSurface.get_width()
             winy = windowSurface.get_height()
             del tempSurface
-            scalew = float(winx)/smallwinx
-            scaleh = float(winy)/smallwiny
-            tilewidth_scaled = round(scaled_variable(tilewidth,scalew))
-            tileheight_scaled = round(scaled_variable(tileheight,scaleh))
-            # i=1
+            scalew = float(winx) / smallwinx
+            scaleh = float(winy) / smallwiny
+            tilewidth_scaled = round(scaled_variable(tilewidth, scalew))
+            tileheight_scaled = round(scaled_variable(tileheight, scaleh))
+
             for row in range(0, nheight):
                 for column in range(0, nwidth):
                     textures[tilemap[row][column]].setXY(column * tilewidth_scaled, row * tileheight_scaled)
                     textures[tilemap[row][column]].scaleValues(scalew, scaleh)
                     # i+=1
                     # print(i)
-                    #print(textures[tilemap[row][column]].image_rect)
+                    # print(textures[tilemap[row][column]].image_rect)
 
             house_scaled = house_obj
             house_scaled.scaleValues(scalew, scaleh)
@@ -246,33 +229,44 @@ while True:
             # print player.Animation_scaled
             # print(scalew,scaleh)
             player.setScale(scalew, scaleh)
-
             danny.setScale(scalew, scaleh)
             # danny.scaleValues()
             # danny.playAnim"down"]
             # danny.move(event, windowSurface)
 
     '''Inter'''
-    for row in range(0,nheight):
-        for column in range(0,nwidth):
-            #print str(row)+","+str(column)
-            #print(tilemap[row][column])
-            #print(block_obj.getCollision(house_obj))
+    for row in range(0, nheight):
+        for column in range(0, nwidth):
+            # print str(row)+","+str(column)
+            # print(tilemap[row][column])
+            # print(block_obj.getCollision(house_obj))
             current_tile = tilemap[row][column]
             textures[current_tile].setXY(round(column * tilewidth_scaled), round(row * tileheight_scaled))
             # print(round(column * tilewidth_scaled),round(row * tileheight_scaled))
-            #textures[tilemap[row][column]].image_rect=(round(column * tilewidth_scaled), round(row * tileheight_scaled), m.ceil(tilewidth_scaled),m.ceil(tileheight_scaled))
-            #print(textures[tilemap[row][column]].image_rect)
+            textures[tilemap[row][column]].image_rect = (
+            round(column * tilewidth_scaled), round(row * tileheight_scaled), m.ceil(tilewidth_scaled),
+            m.ceil(tileheight_scaled))
+            # print(textures[tilemap[row][column]].image_rect)
             windowSurface.blit(textures[current_tile].image_obj, textures[current_tile].image_rect)
 
             if current_tile in block_textures:
                 player.collision_with(textures[current_tile])
+                # if tilemap[row][column] == block:
+                # textures[tilemap[row][column]].coll
+                # print(player.collision_rect,textures[tilemap[row][column]].collision_rect)
 
+            # col_initx1 = player_Rx - hitbox_2
+    # col_initx2 = player_Rx + hitbox_2
+    # col_inity1 = player_Ry - hitbox_2
+    # col_inity2 = player_Ry + hitbox_2
+
+    # pygame.draw.rect(windowSurface, (0,0,0), Rect(player.col, col_inity1,2*hitbox_2,2*hitbox_2))
+    # print(Rect(col_initx1, col_inity1, 2 * hitbox_2, 2 * hitbox_2))
     danny.playAnim(windowSurface, "down")
 
     player.move(event, windowSurface)
     # print(player.x_scaled, player.y_scaled)
+    # print("player " + str(player.collision_rect))
 
     pygame.display.update()
-    mainClock.tick(FPS) 
-
+    mainClock.tick(FPS)
